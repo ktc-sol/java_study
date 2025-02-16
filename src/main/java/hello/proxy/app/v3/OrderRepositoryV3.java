003848095fd3c5f988bcc3062706fcd150fd3c89
82258904 <sol.kang@kt.com>
2025-02-16 21:57:57 +0900

    feat: Day-2. 프록시 패턴 및 데코레이터 패턴 학습

---
+++ src/main/java/hello/proxy/app/v3/OrderRepositoryV3.java
@@ -0,0 +1,23 @@
+package hello.proxy.app.v3;
+
+import org.springframework.stereotype.Repository;
+
+//@Repository
+public class OrderRepositoryV3 {
+
+    public void save(String itemdId) {
+        // 저장 로직
+        if(itemdId.equals("ex")) {
+            throw new IllegalArgumentException();
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
